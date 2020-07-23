7cf429ffd2967da70bd331eadb779f13206df08a
unknown <Shellynguyen98@gmail.com>
2020-07-22 23:37:19 -0700

    change = to :

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ var generateBtn = document.querySelector("#generate");
 
 // // Write password to the #password input
 function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
 
 
 
@@ -28,10 +23,10 @@ let specialConfirm = confirm('Do you want special characters?')
 
 let characters = {
 
-  upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-  lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-  numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
-  special = ['+', '-', '&','!' , '(', ')', '{', '}', '[', ']', '^', '~', '*', '?', ':']
+  upperCase : ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+  lowerCase : ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+  numbers : ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+  special : ['+', '-', '&','!' , '(', ')', '{', '}', '[', ']', '^', '~', '*', '?', ':']
 }
 
 let password = ""
@@ -56,32 +51,26 @@ for (let i = 0; i < passLength; i ++) {
     } else {
       i--
     }
-  }
-
-  else if (pick === 2) {
+  } else if (pick === 2) {
     if (numberConfirm === true) {
       let change = characters.numbers[Math.floor(Math.random() + characters.numbers.length)]
       password= password + change
     } else {
       i--
     }
-  }
-
-  else {
+  } else {
     if (specialConfirm === true) {
       let change = characters.specialCase[Math.floor(Math.random() + characters.specialCase.length)]
       password= password + change
     } else {
       i--
     }
-  } 
+  }
+}
 
-
-    
-    
-    
 document.getElementById('password').innerHTML = `${password}` 
 
-
+}
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
